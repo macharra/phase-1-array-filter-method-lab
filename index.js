fd9c0a5ae5a9b59ed4da9a74f8a6bd05ee736103
rogers.machara <rogersmachara13@gmail.com>
2024-03-28 13:29:28 +0300

    uses the filter method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const findMatching =function (drivers, name) {
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+}
+
+const fuzzyMatch = function(drivers, beginsWith) {
+    return drivers.filter(driver =>
+        driver.toLowerCase().startsWith(beginsWith.toLowerCase())
+    );
+};
+
+
+const matchName = function(drivers, name) {
+    return drivers.filter(driver =>
+        driver.name === name
+    );
+};
